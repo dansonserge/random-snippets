43990e591df6dd710d194d73eb8bb2ef6c63a2b1
Serge Danson <sergedanson@gmail.com>
2022-08-14 11:33:06 +0200

    let, const and template literals

---
+++ javascript/jsNewFeatures.jsx
@@ -0,0 +1,36 @@
+
+//=======================
+//LET and CONST
+
+/* 
+   LET indicates the variable can still be re-assigned to another value, 
+   whille with CONST, we're saying that's the final value
+*/
+
+let favNumber = 7;
+const LargestSea ='The Philippine Sea'
+
+/* 
+    favNumber can still be re-assigned without any issue, 
+    but if you try to re-assign LargestSea, you will get a parser error, Assignment to constant variable.
+*/
+
+const Tenary = ()=>{
+ return (<div className={success?'success-item' :'item'}>
+    Items List
+</div>)}
+
+//=============================
+//Template literals
+
+const ageOutput = `You are ${age} years old `
+
+//instead of the old way
+const ageOutputOld = 'You are ' + age + ' years old'
+
+
+
+
+
+
+
