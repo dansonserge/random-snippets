31b71309ef7f3e5ca9b87f4bf3fdf35d26d61d06
Serge Danson <sergedanson@gmail.com>
2022-08-14 15:25:10 +0200

    using the HOFs in React

---
+++ javascript/hofInReact.jsx
@@ -0,0 +1,39 @@
+const Users= () => {
+    const currenttUserId=3
+    const vipUserPurchase = 10000
+    const raffleUserWinners = [1,4,3]
+    
+    // map
+    const _users = users.map(user => (<p key={user.id}> {user.username} </p>))
+    function reducerFunc(total, currVal){
+        return total + currVal.lifeTimePurchase
+    }
+    
+    //reduce
+    const totalLifeTimePurchases= users.reduce(reducerFunc,0)
+    
+    // find
+    const currentUser = users.find(user=> user.id== currentUserId)
+    
+    //filter
+    const vipList = users.filter(user=> user.lifeTimePurchase >= vipUserPurchase)
+    
+    // includes
+    const isRaffleWinner = users.map(user=>user.id).includes(currenttUserId)
+
+    return (
+            <>
+            {_users}
+            <p>Total Purchase: {totalLifeTimePurchase} </p>
+            <p>current user: {currentUser.username} </p>
+            
+                <h4> vip list <h4>
+                {
+                vipList.map(user=> <p key={user.id}> {user.username} </p>)
+                }
+            
+                raffle status: {isRaffleWinner ? 'Congrats, you're a raffle winner' : 'Oops! Try again later'}
+        
+        </>;
+    );
+}
